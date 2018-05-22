3fd6a69127d8dfcfd805505caed15710ab9b73c1
ODYSSEY-I\andrew.mcinnes <andrew.mcinnes@interact-intranet.com>
2018-05-22 22:37:14 +0100

    Added product and discount services

---
+++ Checkout.Service.Interfaces/IDiscountService.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq;
+using Checkout.Service.Models;
+
+namespace Checkout.Service
+{
+	public interface IDiscountService
+	{
+		IOrderedEnumerable<QuantityDiscount> GetDiscounts();
+
+		IOrderedEnumerable<QuantityDiscount> GetDiscountsByProduct(string product);
+
+		IOrderedEnumerable<QuantityDiscount> GetEligibleDiscounts(string product, int quantity);
+
+		QuantityDiscount GetDiscountByProductQuantity(string product, int quantity);
+	}
+}
---
+++ Checkout.Service.Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Checkout.Service.Models;
+
+namespace Checkout.Service
+{
+	public interface IProductService
+	{
+		IList<Product> GetProductsAlphabetically();
+
+		IList<Product> GetProductsByPrice();
+
+		Product GetProductByName(string name);
+	}
+}
