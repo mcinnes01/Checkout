482a2d920db2300356b03c97ca993723bf6a3da8
ODYSSEY-I\andrew.mcinnes <andrew.mcinnes@interact-intranet.com>
2018-05-22 22:30:34 +0100

    Initial commit - Shell solution

---
+++ Checkout.Service.Interfaces/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Checkout.Service.Interfaces
+{
+    public class Class1
+    {
+    }
+}
---
+++ Checkout.Service/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Checkout.Service
+{
+    public class Class1
+    {
+    }
+}
